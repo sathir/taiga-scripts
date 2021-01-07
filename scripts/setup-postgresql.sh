2c7308ed917d5b29968196388705dfe0179dc9bb
Mansoor M. Sathir <sathir@live.com>
2021-01-07 12:13:10 +0530

    -> Postgresql version error fixed

--- scripts/setup-postgresql.sh
+++ scripts/setup-postgresql.sh
@@ -13,8 +13,8 @@ function dropdb-if-needed {
 }
 
 if [ ! -e ~/.setup/postgresql ]; then
-    apt-install-if-needed postgresql-9.5 postgresql-contrib-9.5 \
-        postgresql-doc-9.5 postgresql-server-dev-9.5
+    apt-install-if-needed postgresql postgresql-client \
+        postgresql-doc-10 postgresql-server-dev-10
 
     sudo -u postgres createuser --superuser $USER &> /dev/null
     sudo -u postgres createdb $USER &> /dev/null
